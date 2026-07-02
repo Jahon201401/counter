04544b9f14961799d89cfbfb0ba5273dcc197d10
Jahon201401 <jjumamuratovcode@gmail.com>
2026-07-02 10:10:59 +0500

    counter

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let count = document.getElementById("count")
+
 let inc = document.getElementById("inc")
+
 let decrement = document.getElementById("decrement")
+
 let reset = document.getElementById("reset")
+
 let number = 0
 
 function yangilash(){
